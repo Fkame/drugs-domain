a2985b08c7f48208449cd0fe4bceecf660e96a5f
Fkame <nikitos_flame@mail.ru>
2022-10-12 22:10:23 +0300

    created MasterParser module

---
+++ MasterParser/src/main/java/ru/drugsdomain/MasterParser/MasterParserApplication.java
@@ -0,0 +1,13 @@
+package ru.drugsdomain.MasterParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterParserApplication.class, args);
+	}
+
+}
---
+++ MasterParser/src/test/java/ru/drugsdomain/MasterParser/MasterParserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.drugsdomain.MasterParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
