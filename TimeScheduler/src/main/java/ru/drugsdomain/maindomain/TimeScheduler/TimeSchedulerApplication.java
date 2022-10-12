55787841caf72d6d30f0dc5c39af48459c290e19
Fkame <nikitos_flame@mail.ru>
2022-10-12 20:27:12 +0300

    init#1

---
+++ TimeScheduler/src/main/java/ru/drugsdomain/maindomain/TimeScheduler/TimeSchedulerApplication.java
@@ -0,0 +1,13 @@
+package ru.drugsdomain.maindomain.TimeScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSchedulerApplication.class, args);
+	}
+
+}
---
+++ TimeScheduler/src/test/java/ru/drugsdomain/maindomain/TimeScheduler/TimeSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.drugsdomain.maindomain.TimeScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
