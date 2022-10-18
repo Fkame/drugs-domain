e1fcef76a9f91351585722a9e685d2ad366410b0
Fkame <nikitos_flame@mail.ru>
2022-10-18 14:46:12 +0300

    add SlaveParser module

---
+++ SlaveParser/src/main/java/ru/drugsdomain/SlaveParser/SlaveParserApplication.java
@@ -0,0 +1,13 @@
+package ru.drugsdomain.SlaveParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaveParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlaveParserApplication.class, args);
+	}
+
+}
